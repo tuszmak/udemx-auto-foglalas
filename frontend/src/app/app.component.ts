4d18d651e01742f24f9579798e2d5d7646df5857
Tuszmak <daniel.pinter02@gmail.com>
2025-03-13 13:07:31 +0100

    feat: design

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { provideNativeDateAdapter } from '@angular/material/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   providers: [provideNativeDateAdapter()],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
